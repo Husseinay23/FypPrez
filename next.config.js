3fe87a6864e7a0f8d3f209a975c0bedf8f256ac3
Hussein Ayoub <husseinay032@gmail.com>
2026-01-14 14:43:51 +0200

    Fix Vercel out

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
